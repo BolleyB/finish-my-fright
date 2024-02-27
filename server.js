12684eba1f2996dc271dea1d82ed568b83a74c6d
aaronfnp <aaronfnp@gmail.com>
2024-02-27 09:36:58 -0500

    added storiesrouter in server

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const storiesRouter = require('./routes/stories')
 
 const app = express();
 
