d35992a2635ed277e9a9505fd067c4efda78c025
aaronfnp <aaronfnp@gmail.com>
2024-02-27 14:46:40 -0500

    delete chapter functionality added

--- controllers/chaptersController.js
+++ controllers/chaptersController.js
@@ -1,7 +1,8 @@
 const Story = require('../models/story');
 
 module.exports = {
-    create
+    create,
+    delete: deleteChapter
 }
 
 async function create(req, res) {
@@ -13,3 +14,15 @@ async function create(req, res) {
     res.redirect(`/stories/${story._id}`);
 }
 
+async function deleteChapter(req, res) {
+    // Need to add the user check
+    const story = await Story.findById(req.params.id);
+
+    if (!story) return res.redirect('/story');
+    // Remove the chapter using the remove method available on Mongoose arrays
+    await story.chapters.remove(req.params.chapterid);
+    // Save the updated movie doc
+    await story.save();
+    // Redirect back to the movie's show view
+    res.redirect(`/stories/${story._id}`);
+  }
--- routes/chapters.js
+++ routes/chapters.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const chaptersCtrl = require('../controllers/chaptersController')
 
 router.post('/stories/:id/chapters', chaptersCtrl.create)
+router.delete('/stories/:id/chapters/:chapterid', chaptersCtrl.delete);
 
 module.exports = router;
