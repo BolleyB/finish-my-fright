0e2e5675aac149b000639f3a378aea396d6e318d
aaronfnp <aaronfnp@gmail.com>
2024-02-27 15:48:31 -0500

    added route to user page

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const usersCtrl = require('../controllers/usersController');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', usersCtrl.index);
 
 module.exports = router;
