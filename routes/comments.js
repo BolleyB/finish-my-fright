6774e1e55e25b3f95624f736bd44a59ad6658b69
aaronfnp <aaronfnp@gmail.com>
2024-02-27 15:59:38 -0500

    added delete comment fuctionality

--- routes/comments.js
+++ routes/comments.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const commentsCtrl = require('../controllers/commentsController')
 
 router.post('/stories/:id/comments', commentsCtrl.create)
-// router.delete('/stories/:id/comments/:commentid', commentsCtrl.delete);
+router.delete('/stories/:id/comments/:commentid', commentsCtrl.delete);
 
 module.exports = router;
