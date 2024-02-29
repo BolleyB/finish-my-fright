513607feb5d9e50d404436ea53dcedacf3adfecd
roguione <myjobcontacts@yahoo.com>
2024-02-29 09:47:42 -0600

    merged module_exports ad getAllStories objects into single object. the stories API is now exposed

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -1,74 +1,63 @@
 const Story = require('../models/story');
+const User = require('../models/user');
+
+async function updateUser(id, storyId) {
+    try {
+        await User.updateOne({ _id: id }, { $push: { 'interaction': { stories: storyId } } });
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
 
 module.exports = {
-  getAllStories: async (req, res) => {
-    try {
-      const stories = await Story.find();
-      res.json(stories);
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
+    getAllStories: async (req, res) => {
+        try {
+            const stories = await Story.find();
+            res.json(stories);
+        } catch (error) {
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    },
+    index: async (req, res) => {
+        const stories = await Story.find({});
+        res.render('stories/index', { title: 'Stories', stories });
+    },
+    new: (req, res) => {
+        res.render('stories/new', { errorMsg: '' });
+    },
+    create: async (req, res) => {
+        req.body.title = req.body.title.trim();
+        req.body.user = req.user.id;
+        try {
+            const newStory = await Story.create(req.body);
+            await updateUser(req.user.id, newStory._id);
+            console.log('Story Created');
+            res.redirect(`/stories/${newStory._id}`);
+        } catch (err) {
+            console.error(err);
+            // res.render('stories/new', { errorMsg: 'Error creating the story.' });
+        }
+    },
+    show: async (req, res) => {
+        try {
+            const story = await Story.findById(req.params.id);
+            res.render('stories/show', { title: 'Story Details', story });
+        } catch (err) {
+            console.log(err);
+            res.redirect('/stories');
+        }
+    },
+    edit: async (req, res) => {
+        const story = await Story.findById(req.params.id);
+        res.render('stories/edit', { story });
+    },
+    update: async (req, res) => {
+        try {
+            await Story.updateOne({ _id: req.params.id }, { body: req.body.body });
+            res.redirect(`/stories/${req.params.id}`);
+        } catch (err) {
+            console.log(err);
+        }
     }
-  },
-  // Implement other controller functions for CRUD operations if needed
 };
-
-// Your existing controller functions
-async function index(req, res) {
-    const stories = await Story.find({});
-    res.render('stories/index', {title: 'Stories', stories});
-}
-
-function newStory(req, res) {
-    res.render('stories/new', { errorMsg: ''});
-}
-
-async function create(req, res) {
-    req.body.title = req.body.title.trim();
-    //adds user to information
-    req.body.user = req.user.id;
-    try {
-        const newStory = await Story.create(req.body);
-        console.log('Story Created');
-        res.redirect(`/stories/${newStory._id}`);
-    } catch (err) {
-        console.error(err);
-        // res.render('stories/new', { errorMsg: 'Error creating the story.' });
-    }
-}
-
-async function show(req, res) {
-    try {
-        const story = await Story.findById(req.params.id);
-        res.render('stories/show', { title: 'Story Details', story})
-    } catch (err) {
-        console.log(err);
-        res.redirect('/stories');
-    }
-}
-
-async function edit(req, res) {
-    const story = await Story.findById(req.params.id);
-
-    res.render('stories/edit', {story})
-}
-
-async function update(req, res) {
-    try {
-        const story = await Story.findById(req.params.id);
-
-        await Story.updateOne({ _id: req.params.id }, { body: req.body.body });
-        res.redirect(`/stories/${req.params.id}`);
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-module.exports = {
-    index,
-    new: newStory,
-    create,
-    show,
-    edit,
-    update
-}
-
