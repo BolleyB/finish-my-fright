b1d015ca607681b180fc831ee02cd48f58e8f82f
aaronfnp <aaronfnp@gmail.com>
2024-02-29 13:18:39 -0500

    fixed routing issues with redirect

--- routes/users.js
+++ routes/users.js
@@ -4,17 +4,17 @@ const router = express.Router();
 const usersCtrl = require('../controllers/usersController');
 const ensureLoggedIn = require('../config/ensureLoggedIn');
 
-/* GET users listing. */
-router.get('/', usersCtrl.index);
+// GET All User Profiles
+router.get('/all', usersCtrl.showAll)
 
 // GET User Redirect
 router.get('/redirect', usersCtrl.redirect);
 
+/* GET users listing. */
+router.get('/', usersCtrl.index);
+
 // GET User Profile
 router.get('/:id', usersCtrl.showOne);
-
-// GET All User Profiles
-router.get('/all', usersCtrl.showAll)
 
 // PUT FOR UPDATING STORY
 router.put('/:id', ensureLoggedIn, usersCtrl.update);
